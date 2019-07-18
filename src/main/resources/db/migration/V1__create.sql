74e67378f66e714e01954916eedd728afc3b4718
DALLIN GUO (ITA-ISDC-ISD-OOCLL/ZHA) <GUODA@OOCL.COM>
2019-07-18 18:25:51 +0800

    second

---
+++ src/main/resources/db/migration/V1__create.sql
@@ -0,0 +1,31 @@
+CREATE TABLE cases (
+caseId int(50),
+caseName varchar(255),
+caseTime  varchar(100)
+);
+
+create Table caseDetail (
+caseDetailId int(50),
+subjectiveDetail varchar(255),
+objectiveDetail varchar(255)
+);
+
+
+CREATE TABLE Justice (
+    justiceId int,
+    name varchar(100),
+    age int(255),
+    sex varchar(10),
+    birthday varchar(50),
+    id_number int(100),
+    experience int(50)
+);
+
+CREATE TABLE Prosecutor (
+    proId int(50),
+    proName Varchar(255),
+ );
+
+
+
+
